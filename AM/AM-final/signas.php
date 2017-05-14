8599fcde7acfecff1c4e2c8ab8cebc36773059a8
johnraeo <2154041@slu.edu.ph>
2017-05-15 01:44:15 +0800

    Fixed signas.php

--- AM/AM-final/signas.php
+++ AM/AM-final/signas.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sign as</title>
+		<link rel="stylesheet" href="styles/navigation.css" />
+		<link rel="stylesheet" href="styles/signas.css" />
+	</head>
+	<body>
+
+<div id= "nav">
+        <nav>
+            <ul class="nav nav-right nav fixed">
+            <li><a href="store.html">STORE</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+                <li><a href="about.html">OUR TEAM</a></li>
+                
+                <li class="dropdown">
+                <a href="#" class="dropbtn">SERVICES</a>
+                <div class="dropdown-content">
+                    <a href="pawgroom.html">PawGroom</a>
+                    <a href="pawmed.html">PawMed</a>
+                </div>
+                </li>
+                <li><a href="index.html">HOME</a></li>
+               
+            </ul>
+        </nav>
+</div>
+
 <link rel="stylesheet" href="styles/signas.css" type="text/css">
 
 <div class="signasa">
@@ -13,3 +43,6 @@
 	</form>
 
 </div>
+
+</body>
+</html>
