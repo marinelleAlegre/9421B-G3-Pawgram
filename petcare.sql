2d78b3317e4b9059ce2fc01ff674a007daa84c55
marinelleAlegre <biku.tomoshimasen@gmail.com>
2017-05-14 14:41:37 +0800

    Added updated database

---
+++ petcare.sql
@@ -0,0 +1,380 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: petcare
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appointment` (
+  `appointment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_date` date NOT NULL,
+  `expected_date` date NOT NULL,
+  `status` enum('Done','Not yet done') NOT NULL,
+  `time` time(4) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `serviceprovider_id` int(11) NOT NULL,
+  `servicerequest_id` int(11) NOT NULL,
+  PRIMARY KEY (`appointment_id`),
+  UNIQUE KEY `appointment_id_UNIQUE` (`appointment_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
+  UNIQUE KEY `serviceprovider_id_UNIQUE` (`serviceprovider_id`),
+  UNIQUE KEY `servicerequest_id_UNIQUE` (`servicerequest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment`
+--
+
+LOCK TABLES `appointment` WRITE;
+/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `chatDate` date NOT NULL,
+  `content` varchar(45) NOT NULL,
+  `serviceprovider_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  PRIMARY KEY (`chat_id`),
+  UNIQUE KEY `serviceprovider_id_UNIQUE` (`serviceprovider_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
+  UNIQUE KEY `chat_id_UNIQUE` (`chat_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat`
+--
+
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) NOT NULL,
+  `dateOfRegistration` date NOT NULL,
+  `pet_type` varchar(45) NOT NULL,
+  `registration_id` int(11) NOT NULL,
+  `customerFirstName` varchar(45) NOT NULL,
+  `customerLastName` varchar(45) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `registration_id_UNIQUE` (`registration_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `time` time(4) NOT NULL,
+  `total_amount` int(6) NOT NULL,
+  `availService_desc` varchar(45) NOT NULL,
+  `invoice_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
+  UNIQUE KEY `payment_id_UNIQUE` (`payment_id`),
+  UNIQUE KEY `invoice_id_UNIQUE` (`invoice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `products_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_description` varchar(45) NOT NULL,
+  `product_name` varchar(45) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`products_id`),
+  UNIQUE KEY `products_id_UNIQUE` (`products_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registration`
+--
+
+DROP TABLE IF EXISTS `registration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registration` (
+  `register_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `age` int(3) NOT NULL,
+  `gender` enum('M','F') NOT NULL,
+  `address` text NOT NULL,
+  `contactNumber` int(11) NOT NULL,
+  `status` enum('Accepted','Declined','Pending') NOT NULL,
+  `typeOfUser` enum('Customer','Service Provider') NOT NULL,
+  PRIMARY KEY (`register_id`),
+  UNIQUE KEY `register_id_UNIQUE` (`register_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration`
+--
+
+LOCK TABLES `registration` WRITE;
+/*!40000 ALTER TABLE `registration` DISABLE KEYS */;
+INSERT INTO `registration` VALUES (11,'Derije','Franchesca Bernadette','Chesca','13134eccf270b34237ed5743eae8d272','derijefranchesca@sample.com',20,'F','Aurora hill',102030405,'Accepted','Customer'),(12,'Calub','Janby Louis','Jaby','92a3adae68adcf6f173808b05c456492','jaby@sample.com',19,'M','Bonifacio Street',1023405561,'Accepted','Customer'),(13,'Siapno','Renz Ivan','ivanpogiit','5f967e862d09cab338511f3ca3a7ddd3','pogiit@sample.com',21,'M','Aurora Hill',909867532,'Accepted','Customer'),(14,'Baucas','Marinel','marinelBaucas','90133b0b17a6bfc5db867690df94afbc','marinel@sample.com',23,'F','Balacbac',1234567890,'Accepted','Customer'),(15,'Cayabyab','Dianne Alyza','dayan','d6890a40e86d719ad665c7984a401bc9','dayan@sample.com',19,'F','La Trinidad',1928374650,'Accepted','Customer');
+/*!40000 ALTER TABLE `registration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicecategory`
+--
+
+DROP TABLE IF EXISTS `servicecategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servicecategory` (
+  `servicecategory_id` int(11) NOT NULL AUTO_INCREMENT,
+  `services` enum('Grooming','Veterinary Services') NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `serviceprovider_id` int(11) NOT NULL,
+  PRIMARY KEY (`servicecategory_id`),
+  UNIQUE KEY `servicecategory_id_UNIQUE` (`servicecategory_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
+  UNIQUE KEY `serviceprovider_id_UNIQUE` (`serviceprovider_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicecategory`
+--
+
+LOCK TABLES `servicecategory` WRITE;
+/*!40000 ALTER TABLE `servicecategory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicecategory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serviceprovider`
+--
+
+DROP TABLE IF EXISTS `serviceprovider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `serviceprovider` (
+  `serviceprovider_id` int(11) NOT NULL AUTO_INCREMENT,
+  `expertise` varchar(45) NOT NULL,
+  `dateOfRegistration` date NOT NULL,
+  `workedStarted` date NOT NULL,
+  `status` enum('Busy','Inactive','Free') NOT NULL,
+  `registration_id` int(11) NOT NULL,
+  `servicerequest_id` int(11) NOT NULL,
+  `serviceproviderFirstName` varchar(45) NOT NULL,
+  `serviceproviderLastName` varchar(45) NOT NULL,
+  PRIMARY KEY (`serviceprovider_id`),
+  UNIQUE KEY `serviceprovider_id_UNIQUE` (`serviceprovider_id`),
+  UNIQUE KEY `registration_id_UNIQUE` (`registration_id`),
+  UNIQUE KEY `servicerequest_id_UNIQUE` (`servicerequest_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serviceprovider`
+--
+
+LOCK TABLES `serviceprovider` WRITE;
+/*!40000 ALTER TABLE `serviceprovider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serviceprovider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicerequest`
+--
+
+DROP TABLE IF EXISTS `servicerequest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servicerequest` (
+  `servicerequest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` enum('Accepted','Declined','Pending') NOT NULL,
+  `service_description` varchar(45) NOT NULL,
+  `service_price` int(6) NOT NULL,
+  PRIMARY KEY (`servicerequest_id`),
+  UNIQUE KEY `servicerequest_id_UNIQUE` (`servicerequest_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicerequest`
+--
+
+LOCK TABLES `servicerequest` WRITE;
+/*!40000 ALTER TABLE `servicerequest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicerequest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `srcs`
+--
+
+DROP TABLE IF EXISTS `srcs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `srcs` (
+  `servicerequest_id` int(11) NOT NULL,
+  `servicecategory_id` int(11) NOT NULL,
+  PRIMARY KEY (`servicerequest_id`),
+  UNIQUE KEY `servicerequest_id_UNIQUE` (`servicerequest_id`),
+  UNIQUE KEY `servicecategory_id_UNIQUE` (`servicecategory_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `srcs`
+--
+
+LOCK TABLES `srcs` WRITE;
+/*!40000 ALTER TABLE `srcs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `srcs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `srsp`
+--
+
+DROP TABLE IF EXISTS `srsp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `srsp` (
+  `servicerequest_id` int(11) NOT NULL,
+  `serviceprovider_id` int(11) NOT NULL,
+  PRIMARY KEY (`servicerequest_id`),
+  UNIQUE KEY `servicerequest_id_UNIQUE` (`servicerequest_id`),
+  UNIQUE KEY `serviceprovider_id_UNIQUE` (`serviceprovider_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `srsp`
+--
+
+LOCK TABLES `srsp` WRITE;
+/*!40000 ALTER TABLE `srsp` DISABLE KEYS */;
+/*!40000 ALTER TABLE `srsp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `invoice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `history` varchar(45) NOT NULL,
+  PRIMARY KEY (`invoice_id`),
+  UNIQUE KEY `invoice_id_UNIQUE` (`invoice_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'petcare'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-14 14:28:48
