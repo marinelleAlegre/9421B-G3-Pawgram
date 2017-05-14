9b9384689f758449441087ee286c041ad68d9cf4
johnraeo <2154041@slu.edu.ph>
2017-05-15 01:09:41 +0800

    Added styles in log-in page

--- AM/AM-final/login.php
+++ AM/AM-final/login.php
@@ -3,9 +3,32 @@
 	<head>
 		<meta charset="utf-8">
 		<title>LOGIN</title>
-		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="styles/navigation.css" />
+		<link rel="stylesheet" href="styles/login.css" />
 	</head>
 	<body>
+
+<div id= "nav">
+        <nav>
+            <ul class="nav nav-right nav fixed">
+            <li><a href="store.html">STORE</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+                <li><a href="about.html">OUR TEAM</a></li>
+                
+                <li class="dropdown">
+                <a href="#" class="dropbtn">SERVICES</a>
+                <div class="dropdown-content">
+                    <a href="pawgroom.html">PawGroom</a>
+                    <a href="pawmed.html">PawMed</a>
+                </div>
+                </li>
+                <li><a href="index.html">HOME</a></li>
+               
+            </ul>
+        </nav>
+</div>
+	
+	
 <?php
 	require('db.php');
 	session_start();
